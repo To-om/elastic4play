b63a7ea278c5a042b49d86b2604635a48851e181
To-om <toom@thehive-project.org>
2018-11-23 12:37:18 +0100

    wip

--- test/org/elastic4play/database/DBFindSpec.scala
+++ test/org/elastic4play/database/DBFindSpec.scala
@@ -1,6 +1,6 @@
 package org.elastic4play.database
 
-import scala.concurrent.ExecutionContext.Implicits.{ global => ec }
+import scala.concurrent.ExecutionContext.Implicits.{ global ⇒ ec }
 import scala.concurrent.Future
 import scala.concurrent.duration._
 
@@ -13,7 +13,7 @@ import akka.actor.ActorSystem
 import akka.stream.Materializer
 import akka.stream.testkit.scaladsl.TestSink
 import com.sksamuel.elastic4s.searches._
-import common.{ Fabricator => F }
+import common.{ Fabricator ⇒ F }
 import org.elasticsearch.search.SearchHitField
 import org.junit.runner.RunWith
 import org.specs2.mock.Mockito
@@ -176,8 +176,8 @@ class DBFindSpec extends PlaySpecification with Mockito {
       val version = 12L
       hit.version returns version
 
-//      val db = mock[DBConfiguration]
-//      val dbfind = new DBFind(pageSize, keepAlive, db, ec, mat)
+      //      val db = mock[DBConfiguration]
+      //      val dbfind = new DBFind(pageSize, keepAlive, db, ec, mat)
       DBUtils.hit2json(hit) must_== (doc +
         ("_id" → JsString(id)) +
         ("_parent" → JsString(parent)) +
